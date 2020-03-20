ef7720aad4e721553a473f2b028deedd7285baa7
Julien Roubieu <jroubieu@gmail.com>
2020-03-20 19:08:25 -0300

    Updated packages. Now on Tensorflow 2.

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-import numpy as np
-import cv2
-from keras.optimizers import SGD
 from models.model1 import CNN_1
 from helpers.loader import load_cards, load_symbols, card_content_to_symbol_names
 
--- models/vgg16.py
+++ models/vgg16.py
@@ -1,54 +1,53 @@
-from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
-import cv2
+import tensorflow as tf
+import numpy as np
 import numpy as np
 
 def VGG_16(input_shape=(300,200,3), weights_path=None):
 
-  model = Sequential()
-  model.add(ZeroPadding2D((1,1), input_shape=input_shape))
-  model.add(Conv2D(64, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(64, (3, 3), activation='relu'))
-  model.add(MaxPooling2D((2,2), strides=(2,2)))
+  model = tf.keras.models.Sequential(
+    tf.keras.layers.ZeroPadding2D((1,1), input_shape=input_shape),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2), strides=(2,2)),
 
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(128, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(128, (3, 3), activation='relu'))
-  model.add(MaxPooling2D((2,2), strides=(2,2)))
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2), strides=(2,2)),
 
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(256, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(256, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(256, (3, 3), activation='relu'))
-  model.add(MaxPooling2D((2,2), strides=(2,2)))
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(256, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(256, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(256, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2), strides=(2,2)),
 
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(MaxPooling2D((2,2), strides=(2,2)))
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2), strides=(2,2)),
 
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(ZeroPadding2D((1,1)))
-  model.add(Conv2D(512, (3, 3), activation='relu'))
-  model.add(MaxPooling2D((2,2), strides=(2,2)))
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.ZeroPadding2D((1,1)),
+    tf.keras.layers.Conv2D(512, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2), strides=(2,2)),
 
-  model.add(Flatten())
-  model.add(Dense(4096, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(4096, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(1000, activation='softmax'))
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(4096, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(4096, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1000, activation='softmax')
+  )
 
   if weights_path:
       model.load_weights(weights_path)
